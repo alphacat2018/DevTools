1008ae6847eb154aa8840a8d46d10c6a1953cdeb
oyyming <oyyming2015@gmail.com>
2017-12-15 11:16:16 +0800

    修改DebugUtils

--- unity/ProjectNecessary/_CustomUtils/Editor/DebugUtils.cs
+++ unity/ProjectNecessary/_CustomUtils/Editor/DebugUtils.cs
@@ -1,87 +1,89 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
-using System.IO;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
 
 /// <summary>
 /// 用于生成新工程的必须文件夹以及导入VSCode
 /// </summary>
 public class DebugUtils : Editor {
 
-
 	#region Make Necessary Folders
 
-	[MenuItem("Utils/Make Necessary Folders %#r")]
+	[MenuItem ("Utils/Make Necessary Folders %#r")]
 	public static void MakeFolders () {
-        List<string> paths = FilterFolderPathList(GetNecessaryFolderPathList());
-		for (int i = 0; i < paths.Count; i++)
-		{
-			Directory.CreateDirectory(paths[i]);
+		List<string> paths = FilterFolderPathList (GetNecessaryFolderPathList ());
+		for (int i = 0; i < paths.Count; i++) {
+			Directory.CreateDirectory (paths[i]);
 		}
 
-        AssetDatabase.Refresh();
-    }
+		// Save Scene
+		SaveMainScene ();
+
+		AssetDatabase.Refresh ();
+	}
 
 	private static List<string> GetNecessaryFolderPathList () {
-        List<string> list = new List<string>();
-        list.Add(Application.dataPath + Path.DirectorySeparatorChar + "Editor");
-        list.Add(Application.dataPath + Path.DirectorySeparatorChar + "Scenes");
-        list.Add(Application.dataPath + Path.DirectorySeparatorChar + "Scripts");
+		List<string> list = new List<string> ();
+		list.Add (Application.dataPath + Path.DirectorySeparatorChar + "Editor");
+		list.Add (Application.dataPath + Path.DirectorySeparatorChar + "Scenes");
+		list.Add (Application.dataPath + Path.DirectorySeparatorChar + "Scripts");
 
-        return list;
-    }
+		return list;
+	}
 
-    private static List<string> FilterFolderPathList(List<string> list)
-    {
-		for (int i = list.Count - 1; i >= 0; i--)
-		{
+	private static List<string> FilterFolderPathList (List<string> list) {
+		for (int i = list.Count - 1; i >= 0; i--) {
 			if (AssetDatabase.IsValidFolder (list[i])) {
-                list.RemoveAt(i);
-            }
+				list.RemoveAt (i);
+			}
 		}
 
-        return list;
-    }
+		return list;
+	}
 
 	#endregion
-
 
 	#region Import VSCode
 
 	[MenuItem ("Utils/Import VSCode %#v")]
 	public static void ImportVSCode () {
 		string path = Application.dataPath + Path.DirectorySeparatorChar + "VSCode";
-        string originalPath = "/Users/oyyming/DevTools/unity/ProjectNecessary/VSCode/";
-        if (AssetDatabase.IsValidFolder(path))
-            return;
-        FileUtil.CopyFileOrDirectory(originalPath, path);
-        AssetDatabase.Refresh();
-    }
+		string originalPath = "/Users/oyyming/DevTools/unity/ProjectNecessary/VSCode/";
+		if (AssetDatabase.IsValidFolder (path))
+			return;
+		FileUtil.CopyFileOrDirectory (originalPath, path);
+		AssetDatabase.Refresh ();
+	}
 
 	#endregion
-
 
 	#region Open File In Finder
 
 	[MenuItem ("Utils/Open In Finder  %#f")]
 	public static void OpenInFinder () {
 		if (Selection.activeObject == null) {
-			Process.Start(Application.dataPath);
-            return;
-        }
+			Process.Start (Application.dataPath);
+			return;
+		}
 
-        string path = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
+		string path = AssetDatabase.GetAssetPath (Selection.activeObject.GetInstanceID ());
 		if (!string.IsNullOrEmpty (path)) {
-            string filePath = Application.dataPath + Path.DirectorySeparatorChar + path.Replace("Assets/", "");
+			string filePath = Application.dataPath + Path.DirectorySeparatorChar + path.Replace ("Assets/", "");
 			if (Directory.Exists (filePath))
-				Process.Start(filePath);
+				Process.Start (filePath);
 			else
-            	Process.Start(Path.GetDirectoryName(filePath));
-        }
-    }
+				Process.Start (Path.GetDirectoryName (filePath));
+		}
+	}
 
 	#endregion
+
+	public static void SaveMainScene () {
+		EditorSceneManager.SaveScene (EditorSceneManager.GetActiveScene (), Application.dataPath + Path.DirectorySeparatorChar + "Scenes" + Path.DirectorySeparatorChar + "Main.unity");
+	}
 
 }
