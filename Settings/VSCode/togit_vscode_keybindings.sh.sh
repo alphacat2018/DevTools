e9ac0f775b3f8be406105b42139399b5c920e121
OuYangYiMing <ymouyang@firewinggames.com>
2018-04-12 10:24:20 +0800

    Update vscode sync shell

---
+++ Settings/VSCode/togit_vscode_keybindings.sh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+srcPath="/Users/oyyming/Library/Application Support/Code/User/keybindings.json"
+desPath="/Users/oyyming/DevTools/Settings/VSCode/keybindings.json"
+
+cp "$srcPath" "$desPath"
---
+++ Settings/VSCode/tolocal_vscode_keybindings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+srcPath="/Users/oyyming/DevTools/Settings/VSCode/keybindings.json"
+desPath="/Users/oyyming/Library/Application Support/Code/User/keybindings.json"
+
+cp "$srcPath" "$desPath"
